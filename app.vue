476b3d99738bb163ca3160da19a9a5dab4cba714
Jocelyn <jocelyntan89@hotmail.com>
2024-05-23 23:35:32 +0200

    feat: add header

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <AppHeader />
     <AppFooter />
   </div>
 </template>
---
+++ components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="py-2 border-b border-gray-300">
+    <div class="container">
+      <a href="" class="block w-fit">
+        <img
+          src="https://iiasa.ac.at/themes/custom/iiasa/standard-logo.svg"
+          alt="logo"
+          class="h-12"
+        />
+      </a>
+    </div>
+  </nav>
+</template>
