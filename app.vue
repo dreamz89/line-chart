d02e1c09acb9de39a46d47fc12515b3315ed9ab7
Jocelyn <jocelyntan89@hotmail.com>
2024-05-23 23:14:56 +0200

    feat: add footer

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <AppFooter />
   </div>
 </template>
---
+++ components/AppFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="bg-black py-8">
+    <div class="container flex gap-x-8">
+      <div class="flex flex-col gap-y-2 text-white max-w-72">
+        <p>
+          International Institute for Applied Systems Analysis (IIASA) |
+          Schlossplatz 1 | A-2361 Laxenburg, Austria
+        </p>
+        <p>T: +43 0000 000 0</p>
+      </div>
+      <div class="flex flex-col gap-y-2 text-white">
+        <a href="" class="w-fit">Media</a>
+        <a href="" class="w-fit">Employment</a>
+        <a href="" class="w-fit">How to find us</a>
+        <a href="" class="w-fit">Whistleblowing Reporting System</a>
+        <a href="" class="w-fit">Cookies</a>
+        <a href="" class="w-fit">Terms of use</a>
+      </div>
+    </div>
+  </footer>
+</template>
