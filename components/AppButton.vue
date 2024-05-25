a2351be682e9257fb07fce7f0edee8bae20d9adb
Jocelyn <jocelyntan89@hotmail.com>
2024-05-25 20:40:33 +0200

    feat: extend button

--- components/AppButton.vue
+++ components/AppButton.vue
@@ -1,14 +1,23 @@
 <template>
-  <button
-    @click="$emit('submit')"
-    class="bg-blue text-white rounded-lg leading-none py-3 px-6 hover:bg-blue-500"
-  >
-    Submit
+  <button @click="$emit('submit')" :disabled="disabled" :class="VARIANTS[type]">
+    {{ label }}
   </button>
 </template>
 
 <script>
 export default {
   emits: ["submit"],
+  props: {
+    disabled: Boolean,
+    label: String,
+    type: String,
+  },
+  created() {
+    this.VARIANTS = {
+      primary:
+        "bg-blue text-white rounded-lg leading-none py-3 px-6 hover:enabled:bg-blue-500 disabled:opacity-30 disabled:cursor-not-allowed",
+      tertiary: "text-blue hover:underline underline-offset-2",
+    }
+  },
 }
 </script>
