a9aed5c9f87890830ea218350ccabcf9a95593a7
Jocelyn <jocelyntan89@hotmail.com>
2024-05-25 20:41:20 +0200

    feat: add select

---
+++ components/AppSelect.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label class="block text-black text-sm mb-1">{{ label }}</label>
+    <USelectMenu
+      class="select-menu"
+      v-model="selected"
+      :options="options"
+      placeholder="Select"
+      size="xl"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["onSelect"],
+  props: {
+    label: String,
+    options: Array,
+    selectedFilter: String,
+  },
+  data() {
+    return {
+      selected: "",
+    }
+  },
+  watch: {
+    selectedFilter() {
+      if (this.selectedFilter !== this.selected) {
+        this.selected = this.selectedFilter
+      }
+    },
+    selected() {
+      this.$emit("onSelect", this.label.toLowerCase(), this.selected)
+    },
+  },
+}
+</script>
